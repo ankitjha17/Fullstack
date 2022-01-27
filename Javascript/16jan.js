79f1e4249ed75308f1c1a43c19a58a37f5574b42
Ankit <ajha00455@gmail.com>
2022-01-27 21:20:18 +0530

    addes js & html

---
+++ Javascript/16jan.js
@@ -0,0 +1,32 @@
+var firstname = "Ankit"
+var Lastname = "Jha"
+var fullname;
+
+console.log(firstname); //print ankit
+console.log(Lastname); //print Jha
+
+console.log(firstname + Lastname); // print ankit jha
+console.log(Lastname + firstname); // print Jha Ankit
+
+
+var first = 2.4
+var second = 6
+
+console.log(first + second); // print 6.4
+console.log (firstname + second); // print Ankit6
+console.log(first + firstname); // print 2.4Ankit
+console.log (first + fullname); // print NAN
+
+// NAN
+//Not a Number
+
+/*
+
++operators
+
+
+String + String = Concat to a String
+    Number + String = Concat to a String
+    Number + Number = Addition to a Number
+    Undefined + String = Concat to a String
+    Undefined + Number = NaN
---
+++ Javascript/2nd.js
@@ -0,0 +1,4 @@
+var i= 5; i=>++ ; j=1; 
+
+for(var i=1; i=>++; j=5)
+console.l
