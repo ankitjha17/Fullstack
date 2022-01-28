2dbdd2bb468979a6791d6bc095229aa29b35c2eb
Ankit <ajha00455@gmail.com>
2022-01-28 20:36:28 +0530

    add some file

---
+++ Javascript/Second.js
@@ -0,0 +1,19 @@
+var firstName = "Ankit";
+var lastName = "Jha";
+var fullName;
+
+//console.log(firstName); // Ankit
+//console.log(lastName); // Jha
+//console.log(fullName); // undefined
+
+
+fullName = firstName + " " + lastName; // New Value is given to Full Name
+
+//console.log(fullName); // Ankit Jha 
+
+
+for(var i = 1; i <=5; i++) {
+    console.log(firstName); // Akshay
+}
+
+
