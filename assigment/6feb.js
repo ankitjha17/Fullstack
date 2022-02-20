47d7f1eec641b90b783b9ebe3cbbe902d32d55d5
Ankit <ajha00455@gmail.com>
2022-02-20 19:34:01 +0530

    commit- 6feb

---
+++ assigment/6feb.js
@@ -0,0 +1,17 @@
+// Assignment
+
+// 3. Convert a given array in descending order using sort method
+
+let Name = ["Santosh", "Amit", "Gaurav", "Brad", "Kishan"];
+console.log(Name);
+console.log(Name.sort().reverse());
+
+/*
+    Before sort- After sort -
+    
+    
+*/
+
+
+let alphabet =[ "d", "c", "r", "s", "z"];
+console.log(alphabet);
