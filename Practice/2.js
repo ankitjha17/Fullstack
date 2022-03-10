57d065b90b34281c1034492e5ddbec14e4bc391d
Ankit <ajha00455@gmail.com>
2022-03-10 21:48:00 +0530

    commit - new js

---
+++ Practice/2.js
@@ -0,0 +1,18 @@
+
+
+// Destructuring (Added in ES-6)
+
+const vehicles = ['Baleno', 'TATA Truck', 'Creta', 'Rickshaw'];
+console.log(vehicles[vehicles.length - 1]);
+// console.log(vehicles[0]);
+
+// Without Destructuring
+
+// const car = vehicles[0];
+// const truck = vehicles[1];
+// const suv = vehicles[2];
+// const rickshaw = vehicles[2];
+
+
+
+
